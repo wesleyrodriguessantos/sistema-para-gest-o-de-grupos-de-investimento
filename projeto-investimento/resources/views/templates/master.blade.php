6ae1fac3871f96ac3190a60402ac1df9bf53141e
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2020-10-13 10:16:24 -0300

    Menu Lateral da página dashboard

--- projeto-investimento/resources/views/templates/master.blade.php
+++ projeto-investimento/resources/views/templates/master.blade.php
@@ -5,10 +5,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Investindo</title>
-  @yield('css-view')
   <link rel="stylesheet" href="{{ asset('css/stylesheet.css') }}">
+  <link href="https://fonts.googleapis.com/css?family=Fredoka+One" rel="stylesheet">
+  <link href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
+  @yield('css-view')
 </head>
 <body>
+  @include('templates.menu-lateral')
   @yield('conteudo-view')
 
   @yield('js-view')
---
+++ projeto-investimento/resources/views/templates/menu-lateral.blade.php
@@ -0,0 +1,46 @@
+<nav id="principal">
+	<ul>
+		<li>
+			<a href="">
+				<i class="fa fa-address-book"></i>
+				<h3>Usuários</h3>
+			</a>
+		</li>
+		<li>
+			<a href="">
+				<i class="fa fa-building"></i>
+				<h3>Instituições</h3>
+			</a>
+		</li>
+		<li>
+			<a href="">
+				<i class="fa fa-users"></i>
+				<h3>Grupos</h3>
+			</a>
+		</li>
+		<li>
+			<a href="">
+				<i class="fa fa-money"></i>
+				<h3>Investir</h3>
+			</a>
+		</li>
+		<li>
+			<a href="">
+				<i class="fa fa-money"></i>
+				<h3>Resgatar</h3>
+			</a>
+		</li>
+		<li>
+			<a href="">
+				<i class="fa fa-dollar"></i>
+				<h3>Aplicações</h3>
+			</a>
+		</li>
+		<li>
+			<a href="">
+				<i class="fa fa-dollar"></i>
+				<h3>Extrato</h3>
+			</a>
+		</li>
+	<ul>
+</nav>
